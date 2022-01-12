9c0ff5d1490b8959e164d4cb7c19e2830b37972b
dell <dell@Nada_Mostafa>
2022-01-12 15:52:32 +0200

    commit

--- src/iwishVIEW/IWISH.java
+++ src/iwishVIEW/IWISH.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package iwishVIEW;
-//new project
+//new projectWWWDD
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
